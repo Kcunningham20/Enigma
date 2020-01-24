7a7b43f5d0f9bbf930755d9bb0e4da466d6ed755
Kristen N Cunningham <kcunningham20@my.whitworth.edu>
2020-01-24 08:40:37 -0800

    Ready for final presentation!

--- enigma.py
+++ enigma.py
@@ -13,6 +13,7 @@ numOfLetters = 0
 def rotor1(letter, startPoint, first, forward, rotorOrder):
 	global route1
 	global numOfLetters
+    #Original Wiring
 	connections = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
 				   ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J']]
 	if startPoint is not None and first:
